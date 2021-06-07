03743edf4b8967d4a065caf8abd44ed3c0559002
markotasic <m4rko2001@gmail.com>
2021-06-07 18:17:59 +0200

    fix

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const darkModeToggle = document.querySelector('.nav__custom-color');
 const body = document.querySelector('.body');
+const goToRegister = document.querySelector('.link-to-register');
+const goToLogin = document.querySelector('.link-to-login');
+const register = document.querySelector('.register');
+const login = document.querySelector('.login');
 
 darkModeToggle.addEventListener('click', () => {
   body.classList.add('darkmode');
+});
+
+goToRegister.addEventListener('click', (e) => {
+  e.preventDefault();
+  register.classList.remove('hidden');
+  login.classList.add('hidden');
+});
+
+goToLogin.addEventListener('click', (e) => {
+  e.preventDefault();
+  register.classList.add('hidden');
+  login.classList.remove('hidden');
 });
