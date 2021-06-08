b37773a92199694d6d73f39314d828baf79f5b3a
markotasic <m4rko2001@gmail.com>
2021-06-08 14:14:57 +0200

    fix

--- script.js
+++ script.js
@@ -21,3 +21,39 @@ darkMode.addEventListener('click', () => {
   body.classList.toggle('darkmode');
 });
 // _____________SWIPE RIGHT FOR SIDEBAR__________________________
+function handleTouchStart(evt) {
+  const firstTouch = getTouches(evt)[0];
+  left = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(evt) {
+  if (!left || !yDown) {
+    return;
+  }
+
+  var xUp = evt.touches[0].clientX;
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = left - xUp;
+  var yDiff = yDown - yUp;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    /*most significant*/
+
+    if (xDiff > 0) {
+      /* left swipe */
+    } else {
+      /* right swipe */
+    }
+  } else {
+    if (yDiff > 0) {
+      /* up swipe */
+    } else {
+      /* down swipe */
+    }
+  }
+  /* reset values */
+  left = null;
+  yDown = null;
+}
