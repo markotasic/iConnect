02c1594ede41ba3948b159f324efcff30b7825e6
markotasic <m4rko2001@gmail.com>
2021-06-08 12:55:52 +0200

    fix

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-const goToRegister = document.querySelector('.link-to-register');
-const goToLogin = document.querySelector('.link-to-login');
-const register = document.querySelector('.register');
-const login = document.querySelector('.login');
+// const goToRegister = document.querySelector('.link-to-register');
+// const goToLogin = document.querySelector('.link-to-login');
+// const register = document.querySelector('.register');
+// const login = document.querySelector('.login');
+const body = document.querySelector('.body');
+const darkMode = document.querySelector('.nav__custom-color');
 
-goToRegister.addEventListener('click', (e) => {
-  e.preventDefault();
-  register.classList.remove('hidden');
-  login.classList.add('hidden');
-});
+// goToRegister.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   register.classList.remove('hidden');
+//   login.classList.add('hidden');
+// });
 
-goToLogin.addEventListener('click', (e) => {
-  e.preventDefault();
-  register.classList.add('hidden');
-  login.classList.remove('hidden');
+// goToLogin.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   register.classList.add('hidden');
+//   login.classList.remove('hidden');
+// });
+
+darkMode.addEventListener('click', () => {
+  body.classList.toggle('darkmode');
 });
