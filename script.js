667a2efb78f107f31fdbcd3a341ef588b40fa891
markotasic <m4rko2001@gmail.com>
2021-06-09 14:08:02 +0200

    fix

--- script.js
+++ script.js
@@ -94,42 +94,36 @@ darkModeToggle.addEventListener('click', () => {
   }
 });
 //__________________________SWIPE RIGHT FOR SIDEBAR__________________________\\
-function handleTouchStart(evt) {
-  const firstTouch = getTouches(evt)[0];
-  left = firstTouch.clientX;
-  yDown = firstTouch.clientY;
-}
 
-function handleTouchMove(evt) {
-  if (!left || !yDown) {
-    return;
+const sidebar = document.querySelector('.sidebar');
+console.log(sidebar.clientWidth);
+const maxLeft = sidebar.clientWidth - 20;
+let startingX = 0;
+let x = 0;
+let diffX = 0;
+let leftOffset = 0;
+let clicked = false;
+sidebar.addEventListener('mousedown', (e) => {
+  startingX = e.offsetX;
+  clicked = true;
+});
+
+document.addEventListener('mouseup', (e) => {
+  clicked = false;
+});
+
+sidebar.addEventListener('mousemove', (e) => {
+  if (clicked) {
+    x = e.offsetX;
+    diffX = startingX < x ? startingX - x : x - startingX;
+    console.log('diffx', diffX);
+    if (diffX < -maxLeft) leftOffset = -maxLeft;
+    else if (diffX > 0) leftOffset = 0;
+    else leftOffset = diffX;
+    console.log('leftoffset', leftOffset);
+    sidebar.style.left = `-${-maxLeft - diffX}px`;
   }
-
-  var xUp = evt.touches[0].clientX;
-  var yUp = evt.touches[0].clientY;
-
-  var xDiff = left - xUp;
-  var yDiff = yDown - yUp;
-
-  if (Math.abs(xDiff) > Math.abs(yDiff)) {
-    /*most significant*/
-
-    if (xDiff > 0) {
-      /* left swipe */
-    } else {
-      /* right swipe */
-    }
-  } else {
-    if (yDiff > 0) {
-      /* up swipe */
-    } else {
-      /* down swipe */
-    }
-  }
-  /* reset values */
-  left = null;
-  yDown = null;
-}
+});
 //__________________________NAV RESPONSIVNES__________________________\\
 $(document).ready(function () {
   $('#search-button').click(function () {
