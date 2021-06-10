7c8d4ca2ee79df0633217c01c05f16f6a767f30f
yournightmare01 <zlatkor1001@gmail.com>
2021-06-10 19:46:47 +0200

    lalala

---
+++ src/register.js
@@ -0,0 +1,16 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+/*const auth = firebase.auth();
+
+function signUp() {
+  let email = document.getElementById('email');
+  let password = document.getElementById('password');
+
+  const promise = auth.createUserWithEmailAndPassword(
+    email.value,
+    password.value
+  );
+  promise.catch((e) => alert(e.message));
+  alert('Singed In');
+}*/
