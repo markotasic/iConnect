b75d8169d2299b45d30840a549b74034a8b7ff78
markotasic <m4rko2001@gmail.com>
2021-06-10 20:04:59 +0200

    fix

--- src/login.js
+++ src/login.js
@@ -1,34 +1,34 @@
-const submit = document.querySelector('.btn-submit');
-const emailInput = document.querySelector('.login__container-mail');
-const passwordInput = document.querySelector('.login__container-password');
+// const submit = document.querySelector('.btn-submit');
+// const emailInput = document.querySelector('.login__container-mail');
+// const passwordInput = document.querySelector('.login__container-password');
 
-submit.addEventListener('click', function (e) {
-  e.preventDefault();
+// submit.addEventListener('click', function (e) {
+//   e.preventDefault();
 
-  // Progres bar dok se loaduje main page
+//   // Progres bar dok se loaduje main page
 
-  let emailValue = emailInput.value;
-  console.log(emailValue);
+//   let emailValue = emailInput.value;
+//   console.log(emailValue);
 
-  let passwordValue = passwordInput.value;
-  console.log(passwordValue);
+//   let passwordValue = passwordInput.value;
+//   console.log(passwordValue);
 
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(emailValue, passwordValue)
-    .then((userCredential) => {
-      // Signed in
-      var user = userCredential.user;
-      // ...
-    })
-    .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-    });
-});
+//   firebase
+//     .auth()
+//     .signInWithEmailAndPassword(emailValue, passwordValue)
+//     .then((userCredential) => {
+//       // Signed in
+//       var user = userCredential.user;
+//       // ...
+//     })
+//     .catch((error) => {
+//       var errorCode = error.code;
+//       var errorMessage = error.message;
+//     });
+// });
 
-// 1. da li su uneti podaci pravilni
-// 2. da li je uneto svako polje || ako nije throw error
-// 3. da li je email dobrog formata
-// 4. submit
-// 5. go to main page
+// // 1. da li su uneti podaci pravilni
+// // 2. da li je uneto svako polje || ako nije throw error
+// // 3. da li je email dobrog formata
+// // 4. submit
+// // 5. go to main page
