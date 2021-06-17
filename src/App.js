748ecf6ee7738105f6b05065a73385dcdef04fc1
yournightmare01 <zlatkor1001@gmail.com>
2021-06-17 22:04:08 +0200

    lalala

--- src/App.js
+++ src/App.js
@@ -1,2 +1,24 @@
 const BASE_URL = 'https://dummyapi.io/data/api';
 const APP_ID = '60c8e11f3722f27da37a9b90';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { App } from './app';
+
+const GQL_URI = 'https://dummyapi.io/data/graphql';
+
+const client = new ApolloClient({
+  uri: GQL_URI,
+  cache: new InMemoryCache(),
+  headers: {
+    'app-id': '60c8e11f3722f27da37a9b90',
+  },
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
