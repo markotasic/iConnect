c8bd36bd364a9873c2e6505309ead0e3cdc2d57a
markotasic <m4rko2001@gmail.com>
2021-06-30 23:01:47 +0200

    yello

--- src/main-page/firebasemain.js
+++ src/main-page/firebasemain.js
@@ -211,9 +211,6 @@ firebase
   .get()
   .then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, ' => ', doc.data());
-
       const HTMLinner = `
       <div class="main__content">
         <div class="main__content-poster">
